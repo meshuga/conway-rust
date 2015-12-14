550ab090382dcf4cfe2473f2908a828a021dda06
Patryk Orwat <patrykorwat@gmail.com>
2015-12-15 00:58:59 +0100

    implemented making new generation

--- src/conway.rs
+++ src/conway.rs
@@ -5,12 +5,12 @@ pub enum LifeIndicator {
 
 pub struct Generation {
     world_map: Vec<Vec<LifeIndicator>>,
-    size: i32,
+    size: usize,
 }
 
 impl Generation {
 
-    pub fn new(world_map: Vec<Vec<LifeIndicator>>, size: i32) -> Generation {
+    pub fn new(world_map: Vec<Vec<LifeIndicator>>, size: usize) -> Generation {
         Generation {
             world_map: world_map,
             size: size,
@@ -31,6 +31,7 @@ impl Generation {
             }
             println!("");
         }
+        println!("");
     }
 
     fn print_cell(&self, cell: &LifeIndicator) {
@@ -39,6 +40,55 @@ impl Generation {
             &LifeIndicator::Dead => print!("_"),
         }
     }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+}
+
+pub struct WorldRules;
+
+impl WorldRules {
+    pub fn should_be_alive(neighbours_count: usize, currently_exists: bool) -> bool {
+        match currently_exists {
+            true => !(neighbours_count < 2 || neighbours_count > 3),
+            false => neighbours_count == 3,
+        }
+    }
+
+    pub fn count_neighbours(generation: &Generation, i: isize, j: isize) -> usize {
+        let mut existing_neighbours_count = 0 as usize;
+
+        for x in (i-1)..(i+2) {
+            for y in (j-1)..(j+2) {
+                if x == i && y == j {
+                    continue;
+                }
+
+                if is_cell_alive_with_shifting(generation, x, y) {
+                    existing_neighbours_count += 1;
+                }
+            }
+        }
+
+        existing_neighbours_count
+    }
+}
+
+fn is_cell_alive_with_shifting(generation: &Generation, mut x_coordinate: isize, mut y_coordinate: isize) -> bool {
+    if x_coordinate < 0 {
+        x_coordinate = generation.size() as isize - 1;
+    } else if x_coordinate == generation.size() as isize {
+        x_coordinate = 0;
+    }
+
+    if y_coordinate < 0 {
+        y_coordinate = generation.size() as isize - 1;
+    } else if y_coordinate == generation.size() as isize {
+        y_coordinate = 0;
+    }
+
+    generation.cell_alive(x_coordinate as usize, y_coordinate as usize)
 }
 
 #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,57 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::io;
 
 pub mod conway;
+use conway::{Generation, LifeIndicator, WorldRules};
 
 fn main() {
-    let path = Path::new("initialMap.txt");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
-        Ok(file) => file,
+    let file_name = "initialMap.txt";
+    let mut generation: Generation = match build_world(file_name) {
+        Err(why) => panic!("Problem with parsing the file {}: {}",file_name, why ),
+        Ok(generation) => generation,
     };
+    generation.print_world();
+    for _x in 0..120 {
+        generation = build_new_generation(&generation);
+        generation.print_world();
+    }
+}
+
+fn build_world(file_name: &str) -> io::Result<Generation> {
+    let path = Path::new(file_name);
+
+    let mut file = try!(File::open(&path));
 
     let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
-        Ok(_) => (),
-    }
+    try!(file.read_to_string(&mut s));
 
     let world_map = s.split("\n")
         .map(|line| line.chars()
             .map(|character| match character {
-                '*' => conway::LifeIndicator::Alive,
-                '_' => conway::LifeIndicator::Dead,
+                '*' => LifeIndicator::Alive,
+                '_' => LifeIndicator::Dead,
                 _ => panic!("Wrong cell value"),
             })
             .collect())
         .collect();
 
-    let generation = conway::Generation::new(world_map, 1);
-    generation.print_world();
+    Ok(Generation::new(world_map, 30))
+}
+
+fn build_new_generation(generation: &Generation) -> Generation {
+    let world_map: Vec<Vec<LifeIndicator>> = (0..generation.size())
+        .map(|i| (0..generation.size())
+            .map(|j| {
+                let count = WorldRules::count_neighbours(&generation, i as isize, j as isize);
+                let currently_exists = generation.cell_alive(i, j);
+
+                match WorldRules::should_be_alive(count, currently_exists) {
+                    true => LifeIndicator::Alive,
+                    false => LifeIndicator::Dead,
+                }
+            }).collect()
+        ).collect();
+    Generation::new(world_map, generation.size())
 }
